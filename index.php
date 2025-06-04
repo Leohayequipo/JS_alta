4e1b1d072f4848add732cab572cd053465f56a61
Leohayequipo <lic.mannanice@gmail.com>
2025-06-04 15:56:58 -0300

    Primer commit index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario de Contacto</title>
+</head>
+<body>
+    <h1>Formulario de Contacto</h1>
+
+    <form id="contactoForm">
+        <input type="text" id="nombre" placeholder="Nombre"><br><br>
+        <input type="email" id="email" placeholder="Email"><br><br>
+        <input type="tel" id="telefono" placeholder="Teléfono"><br><br>
+        <input type="number" id="edad" placeholder="Edad"><br><br>
+        <textarea id="comentario" placeholder="Comentario"></textarea><br><br>
+        <button type="submit">Enviar</button>
+    </form>
+
+    <script src="script.js"></script>
+</body>
+</html>
